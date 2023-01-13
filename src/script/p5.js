8b4eef9a3ca3895e70d57faa08bd38f509ad02a6
João Gomes <86986385+joaoeduardogomes@users.noreply.github.com>
2023-01-13 15:42:38 -0300

    p5-nav
    added img hover effect (with JS)

---
+++ src/script/p5.js
@@ -0,0 +1,79 @@
+//  HOVER JOKER:
+const protagonist5Profile = document.getElementById('p5Protagonist-profile');
+const protagonist5ProfileImg = document.querySelector('#p5Protagonist-profile > .profile-img');
+protagonist5Profile.addEventListener('mouseover', function() {
+    protagonist5ProfileImg.src = "../img/avatars/p5-joker2.png";
+});
+protagonist5Profile.addEventListener('mouseout', function() {
+    protagonist5ProfileImg.src = "../img/avatars/p5-joker1.png";
+});
+
+// HOVER SKULL:
+const ryujiProfile = document.getElementById('ryuji-profile');
+const ryujiProfileImg = document.querySelector('#ryuji-profile > .profile-img');
+ryujiProfile.addEventListener('mouseover', function() {
+    ryujiProfileImg.src = "../img/avatars/p5-ryuji2.png";
+});
+ryujiProfile.addEventListener('mouseout', function() {
+    ryujiProfileImg.src = "../img/avatars/p5-ryuji1.png";
+});
+
+// HOVER PANTHER:
+const annProfile = document.getElementById('ann-profile');
+const annProfileImg = document.querySelector('#ann-profile > .profile-img');
+annProfile.addEventListener('mouseover', function() {
+    annProfileImg.src = "../img/avatars/p5-ann2.png";
+});
+annProfile.addEventListener('mouseout', function() {
+    annProfileImg.src = "../img/avatars/p5-ann1.png";
+});
+
+// HOVER FOX:
+const yusukeProfile = document.getElementById('yusuke-profile');
+const yusukeProfileImg = document.querySelector('#yusuke-profile > .profile-img');
+yusukeProfile.addEventListener('mouseover', function() {
+    yusukeProfileImg.src = "../img/avatars/p5-yuusuke2.png";
+});
+yusukeProfile.addEventListener('mouseout', function() {
+    yusukeProfileImg.src = "../img/avatars/p5-yuusuke1.png";
+});
+
+// HOVER QUEEN:
+const makotoProfile = document.getElementById('makoto-profile');
+const makotoProfileImg = document.querySelector('#makoto-profile > .profile-img');
+makotoProfile.addEventListener('mouseover', function() {
+    makotoProfileImg.src = "../img/avatars/p5-makoto2.png";
+});
+makotoProfile.addEventListener('mouseout', function() {
+    makotoProfileImg.src = "../img/avatars/p5-makoto1.png";
+});
+
+// HOVER ORACLE:
+const futabaProfile = document.getElementById('futaba-profile');
+const futabaProfileImg = document.querySelector('#futaba-profile > .profile-img');
+futabaProfile.addEventListener('mouseover', function() {
+    futabaProfileImg.src = "../img/avatars/p5-futaba2.png";
+});
+futabaProfile.addEventListener('mouseout', function() {
+    futabaProfileImg.src = "../img/avatars/p5-futaba1.png";
+});
+
+// HOVER NOIR:
+const haruProfile = document.getElementById('haru-profile');
+const haruProfileImg = document.querySelector('#haru-profile > .profile-img');
+haruProfile.addEventListener('mouseover', function() {
+    haruProfileImg.src = "../img/avatars/p5-haru2.png";
+});
+haruProfile.addEventListener('mouseout', function() {
+    haruProfileImg.src = "../img/avatars/p5-haru1.png";
+});
+
+// HOVER CROW:
+const akechiProfile = document.getElementById('goro-profile');
+const akechiProfileImg = document.querySelector('#goro-profile > .profile-img');
+akechiProfile.addEventListener('mouseover', function() {
+    akechiProfileImg.src = "../img/avatars/p5-akechi2.png";
+});
+akechiProfile.addEventListener('mouseout', function() {
+    akechiProfileImg.src = "../img/avatars/p5-akechi1.png";
+});
